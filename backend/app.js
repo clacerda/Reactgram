57afdea94091ee8d18eed9f89944d535c411a843
clacerda <claudiovieira_jr@hotmail.com>
2022-11-25 20:14:12 -0300

    config. cors and upload

--- backend/app.js
+++ backend/app.js
@@ -12,6 +12,12 @@ const app = express();
 app.use(express.json())
 app.use(express.urlencoded({extended: false}))
 
+// solve cors
+app.use(cors({credentials:true, origin: "http://localhost:3000"}))
+
+//upload directory
+app.use("/uploads", express.static(path.join(__dirname, "/uploads")))
+
 //routes
 const router = require("./routes/Router.js")
 app.use(router)
