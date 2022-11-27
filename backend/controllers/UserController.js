ce0ef2b63ecdbf360aae2114df437f17ecd4d5ba
clacerda <claudiovieira_jr@hotmail.com>
2022-11-27 17:48:55 -0300

    Config user controller

--- backend/controllers/UserController.js
+++ backend/controllers/UserController.js
@@ -13,7 +13,35 @@ const generateToken = (id) => {
 
 // Register user and sign in
 const register = async(req, res) => {
-    res.send("Registro")
+    const {name, email, password} = req.body
+
+    //check if user exists
+    const user = await User.findOne({email});
+
+    if (user) {
+        res.status(422).json({errors: ["E-mail já utilizado"]});
+    }
+
+    //Generate password Hash
+    const salt = await bcrypt.genSalt();
+    const passwordHash = await bcrypt.hash(password, salt);
+
+    //Create user 
+    const newUser = await User.create({
+        name,
+        email,
+        password: passwordHash
+    });
+
+    //If user was created sucessfully, return token
+    if(!newUser){
+        res.status(422).json({errors: ["Ocorreu um erro, por favor tente mais tarde"]});
+    }
+
+    res.status(201).json({
+        _id: newUser.id,
+        token: generateToken(newUser._id)
+    })
 };
 
 module.exports = {
