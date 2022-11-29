a0a8126d24f99aa3d64c2039e50bfb9071dcd9e0
clacerda <claudiovieira_jr@hotmail.com>
2022-11-29 19:56:59 -0300

    Recover new photo

--- backend/controllers/PhotoController.js
+++ backend/controllers/PhotoController.js
@@ -1,14 +1,31 @@
 const Photo = require("../models/Photo");
 const mongoose = require("mongoose");
+const User = require("../models/User");
 
 //Insert a photo with a user related to it
 const insertPhoto = async(req, res) => {
     const {title} = req.body;
     const image = req.file.filename;
 
-    console.log(req.body);
+    const reqUser = req.user;
 
-    res.send("Photo inserted");
+    const user = await User.findById(reqUser._id);
+
+    //Create a photo
+
+    const newPhoto = await Photo.create({
+        image,
+        title,
+        userId: user._id,
+        userName: user.name,
+    });
+
+    // if photo was created successfully, return data
+    if(!newPhoto){
+        res.status(422).json({errors: "Houve um problema, por favor, tente novamente mais tarde!"});
+    }
+
+    res.status(201).json(newPhoto);
 }
 
 module.exports = {
