ed51374838bd7aa3026af327b82390f8b2dfc44f
clacerda <claudiovieira_jr@hotmail.com>
2022-11-30 18:36:12 -0300

    Recover new photo

--- backend/routes/PhotosRoutes.js
+++ backend/routes/PhotosRoutes.js
@@ -2,7 +2,7 @@ const express = require("express");
 const router = express.Router();
 
 //Controller
-const {insertPhoto} = require("../controllers/PhotoController");
+const {insertPhoto, deletePhoto} = require("../controllers/PhotoController");
 
 //Middlewares
 const {photoInsertValidation} = require("../middlewares/photoValidation");
@@ -11,6 +11,7 @@ const validate = require("../middlewares/handleValidations");
 const {imageUpload} = require("../middlewares/imageUpload");
 
 //Routes
-router.post("/", authGuard, imageUpload.single("image"), photoInsertValidation(), validate, insertPhoto )
+router.post("/", authGuard, imageUpload.single("image"), photoInsertValidation(), validate, insertPhoto);
+router.delete("/:id", authGuard, deletePhoto )
 
 module.exports = router;
