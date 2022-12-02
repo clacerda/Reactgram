f5bb53187ecb84d73d1bdc1cbd50c52bffad9e91
clacerda <claudiovieira_jr@hotmail.com>
2022-12-02 20:41:40 -0300

    Comments a photo

--- backend/controllers/PhotoController.js
+++ backend/controllers/PhotoController.js
@@ -141,10 +141,40 @@ const updatePhoto = async(req, res) => {
     photo.likes.push(reqUser._id);
     photo.save();
     res.status(200).json({photoId: id, userId: reqUser._id, message: "A foto foi curtida."});
-
-
 };
 
+
+    //Comment photos
+    const commentPhoto = async(req, res) => {
+    const {id} = req.params;
+    const {comment} = req.body;
+
+    const reqUser = req.user;
+
+    const user = await User.findById(reqUser._id);
+    const photo = await Photo.findById(id);
+
+    //Check photo exists
+    if (!photo) {
+        res.status(404).json({errors: ["Foto não encontrada."]});
+        return;
+    }
+
+    //put comment in the array of comments
+    const userComment = {
+        comment,
+        userName: user.name,
+        userImage: user.profileImage,
+        userId: user._id
+    };
+
+    photo.comments.push(userComment);
+
+    await photo.save();
+
+    res.status(200).json({comment: userComment, message: "O comentário foi adicionado com sucesso."});
+
+};
 
 module.exports = {
     insertPhoto,
@@ -154,4 +184,5 @@ module.exports = {
     getPhotoById,
     updatePhoto,
     likePhoto,
+    commentPhoto,
 };
--- backend/routes/PhotosRoutes.js
+++ backend/routes/PhotosRoutes.js
@@ -2,10 +2,18 @@ const express = require("express");
 const router = express.Router();
 
 //Controller
-const {insertPhoto, deletePhoto, getAllPhotos, getUserPhotos, getPhotoById, updatePhoto, likePhoto} = require("../controllers/PhotoController");
+const {insertPhoto, 
+       deletePhoto, 
+       getAllPhotos, 
+       getUserPhotos, 
+       getPhotoById, 
+       updatePhoto, 
+       likePhoto, 
+       commentPhoto
+    } = require("../controllers/PhotoController");
 
 //Middlewares
-const {photoInsertValidation, photoUpdateValidation} = require("../middlewares/photoValidation");
+const {photoInsertValidation, photoUpdateValidation, commentValidation} = require("../middlewares/photoValidation");
 const authGuard = require("../middlewares/authGuard");
 const validate = require("../middlewares/handleValidations");
 const {imageUpload} = require("../middlewares/imageUpload");
@@ -19,5 +27,6 @@ router.delete("/:id", authGuard, deletePhoto );
 router.get("/:id", authGuard, getPhotoById);
 router.put("/:id", authGuard, photoUpdateValidation(), validate, updatePhoto, );
 router.put("/like/:id", authGuard, likePhoto );
+router.put("/comment/:id", authGuard, commentValidation(), validate, commentPhoto );
 
 module.exports = router;
