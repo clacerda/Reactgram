31c89a75bef2d340624d0d3b241891bf2a690c59
clacerda <claudiovieira_jr@hotmail.com>
2022-12-06 19:41:42 -0300

    Home, Register and Login

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -2,13 +2,19 @@ import './App.css';
 
 //Router
 import {BrowserRouter, Routes, Route, Navigate} from 'react-router-dom';
+//Pages
+import Home from './pages/Home/Home';
+import Login from './pages/Auth/Login';
+import Register from './pages/Auth/Register';
 
 function App() {
   return (
     <div className="App">
       <BrowserRouter>
         <Routes>
-          <Route path='/' element={}/>
+          <Route path='/' element={<Home />}/>
+          <Route path='/login' element={<Login />}/>
+          <Route path='/register' element={<Register />}/>
         </Routes>
       </BrowserRouter>
     </div>
