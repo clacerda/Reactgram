d64efc7fd1d2d1d3e54576f204bb94df9dae79cd
clacerda <claudiovieira_jr@hotmail.com>
2022-12-07 18:57:16 -0300

    html Register

--- frontend/src/pages/Auth/Register.js
+++ frontend/src/pages/Auth/Register.js
@@ -1,9 +1,36 @@
 import "./Auth.css";
 
+//components
+import {Link} from 'react-router-dom'
+
+//Hooks
+import { useState, useEffect } from "react";
+
+
+
 const Register = () => {
+  const handleSubmit = (e) => {
+    e.preventDefault();
+  };
+
   return (
-    <div>Register</div>
-  )
-}
+    <div>
+      <h2>ReactGram</h2>
+      <p className="subtitle">Cadastre-se para acompanhar seus amigos</p>
+      <form onSubmit={handleSubmit}>
+      <input type="text" placeholder="Nome" />
+      <input type="email" placeholder="E-mail" />
+      <input type="password" placeholder="Senha" />
+      <input type="password" placeholder="Confirme a senha" />
+      <input type="submit" value="Cadastrar" />
+      </form> 
+
+    <p>
+      já tem conta? <Link to="/">Clique aqui.</Link>
+    </p>
+
+    </div>
+  );
+};
 
 export default Register
