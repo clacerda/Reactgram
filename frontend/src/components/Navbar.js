4ada08b740b50c0aa6af6c7ab95a847220c19861
clacerda <claudiovieira_jr@hotmail.com>
2022-12-13 20:01:03 -0300

    Navbar use icons Bs..

--- frontend/src/components/Navbar.js
+++ frontend/src/components/Navbar.js
@@ -1,33 +1,67 @@
 import "./Navbar.css";
 //components
 import { NavLink, Link } from "react-router-dom";
-import {BsSearch, BsHouseDoorFill, BsFillPersonFill, BsFillCameraFill} from 'react-icons/bs'
+import { BsSearch, BsHouseDoorFill, BsFillPersonFill, BsFillCameraFill } from 'react-icons/bs';
+
+// Hooks
+import { useState } from 'react';
+import { useAuth } from "../hooks/userAuth";
+import { useDispatch, useSelector } from 'react-redux';
+import { useNavigate } from "react-router-dom";
 
 const Navbar = () => {
-  return  <nav id="nav">
-            <Link to="/">ReactGram</Link>
-            <form id="search-form">
-                <BsSearch />
-                <input type="text" placeholder="pesquisar..."/>
-            </form>
-            <ul id="nav-links">
-                <li>
-                <NavLink to="/">
-                    <BsHouseDoorFill />
-                </NavLink>
-                </li>
-                <li>
-                <NavLink to="/login">
-                    Entrar
-                </NavLink>
-                </li>
-                <li>
-                <NavLink to="/register">
-                    Cadastrar
-                </NavLink>
-                </li>
-            </ul>
-         </nav>
+
+    const { auth } = useAuth();
+    const { user } = useSelector((state) => state.auth);
+
+    return <nav id="nav">
+        <Link to="/">ReactGram</Link>
+        <form id="search-form">
+            <BsSearch />
+            <input type="text" placeholder="pesquisar..." />
+        </form>
+        <ul id="nav-links">
+            {auth ? (
+                <>
+                    <li>
+                        <NavLink to="/">
+                            <BsHouseDoorFill />
+                        </NavLink>
+                    </li>
+                    {user && (
+                        <li>
+                            <NavLink to={`/users/${user._id}`}>
+                                <BsFillCameraFill />
+                            </NavLink>
+                        </li>
+                    )}
+                    <li>
+                        <NavLink to="/profile">
+                            <BsFillPersonFill />
+                        </NavLink>
+                    </li>
+                    <li>
+                        <span>Sair</span>
+                    </li>
+                </>
+            ) : (
+                <>
+                    <li>
+                        <NavLink to="/login">
+                            Entrar
+                        </NavLink>
+                    </li>
+                    <li>
+                        <NavLink to="/register">
+                            Cadastrar
+                        </NavLink>
+                    </li>
+                </>
+            )}
+
+
+        </ul>
+    </nav>
 }
 
 export default Navbar;
