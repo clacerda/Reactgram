9ed5e5e6d721945782a7c2fabf5f2fad44ab3fbf
clacerda <claudiovieira_jr@hotmail.com>
2022-12-14 19:58:25 -0300

    Login

--- frontend/src/pages/Auth/Login.js
+++ frontend/src/pages/Auth/Login.js
@@ -1,8 +1,34 @@
 import "./Auth.css";
 
+// Components
+import { Link } from "react-router-dom";
+import Message from "../../components/Message";
+
+// Hooks
+import { useEffect, useState } from "react";
+import { useSelector, useDispatch } from 'react-redux';
+
 const Login = () => {
+
+   const [email, setEmail] = useState("");
+   const [password, setPassword] = useState("");
+
+
+  const handleSubmit = (e) => {
+    e.preventDefault();
+  }
+
   return (
-    <div>Auth</div>
+    <div id="login">
+      <h2>ReactGram</h2>
+      <p className="subtitle">Faça o login para ver as novidades.</p>
+      <form onSubmit={handleSubmit}>
+        <input type="text" name="" id="" placeholder="E-mail" onChange={(e) => setEmail(e.target.value)} value={email || ""} />
+        <input type="password" name="" id="" placeholder="Senha" onChange={(e) => setPassword(e.target.value)} value={password || ""}/>
+        <input type="submit" value="Entrar" />
+      </form>
+      <p>Não tem uma conta? <Link to="/register">clique aqui.</Link></p>
+    </div>
   )
 }
 
