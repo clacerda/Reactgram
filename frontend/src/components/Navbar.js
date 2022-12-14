17887982d530fe9b41b1b0f92f74d63915e5b270
clacerda <claudiovieira_jr@hotmail.com>
2022-12-14 19:49:32 -0300

    Loggout

--- frontend/src/components/Navbar.js
+++ frontend/src/components/Navbar.js
@@ -9,10 +9,23 @@ import { useAuth } from "../hooks/userAuth";
 import { useDispatch, useSelector } from 'react-redux';
 import { useNavigate } from "react-router-dom";
 
+// Redux
+import {logout, reset} from '../slices/authSlice';
+
 const Navbar = () => {
 
     const { auth } = useAuth();
     const { user } = useSelector((state) => state.auth);
+    const navigate = useNavigate();
+    const dispatch = useDispatch();
+
+    const handleLogout = () => {
+        dispatch(logout());
+        dispatch(reset());
+
+        navigate("/login");
+
+    }
 
     return <nav id="nav">
         <Link to="/">ReactGram</Link>
@@ -41,7 +54,7 @@ const Navbar = () => {
                         </NavLink>
                     </li>
                     <li>
-                        <span>Sair</span>
+                        <span onClick={handleLogout}>Sair</span>
                     </li>
                 </>
             ) : (
