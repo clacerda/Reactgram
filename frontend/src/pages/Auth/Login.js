8b3c24bb5cfb0c7971c34336aca90d6fac5767cc
clacerda <claudiovieira_jr@hotmail.com>
2022-12-15 18:59:38 -0300

    ajuste bug - acesso

--- frontend/src/pages/Auth/Login.js
+++ frontend/src/pages/Auth/Login.js
@@ -6,52 +6,63 @@ import Message from "../../components/Message";
 
 // Hooks
 import { useEffect, useState } from "react";
-import { useSelector, useDispatch } from 'react-redux';
+import { useSelector, useDispatch } from "react-redux";
 
 // Redux
 import { login, reset } from "../../slices/authSlice";
 
 const Login = () => {
+  const [email, setEmail] = useState("");
+  const [password, setPassword] = useState("");
 
-   const [email, setEmail] = useState("");
-   const [password, setPassword] = useState("");
+  const dispatch = useDispatch();
 
-      const dispatch = useDispatch();
+  const { loading, error } = useSelector((state) => state.auth);
 
-   const {loading, error} = useSelector((state) => state.auth);
-
- 
   const handleSubmit = (e) => {
     e.preventDefault();
 
     const user = {
       email,
-      password
-    }
+      password,
+    };
+
+    console.log(user);
 
     dispatch(login(user));
   };
 
-  // Clean all auth state
+  // Clean all auth states
   useEffect(() => {
-      dispatch(reset())
+    dispatch(reset());
   }, [dispatch]);
 
   return (
     <div id="login">
       <h2>ReactGram</h2>
-      <p className="subtitle">Faça o login para ver as novidades.</p>
+      <p className="subtitle">Faça o login para ver o que há de novo.</p>
       <form onSubmit={handleSubmit}>
-        <input type="text" name="" id="" placeholder="E-mail" onChange={(e) => setEmail(e.target.value)} value={email || ""} />
-        <input type="password" name="" id="" placeholder="Senha" onChange={(e) => setPassword(e.target.value)} value={password || ""}/>
-          
+        <input
+          type="text"
+          placeholder="E-mail"
+          onChange={(e) => setEmail(e.target.value)}
+          value={email}
+        />
+        <input
+          type="password"
+          placeholder="Senha"
+          onChange={(e) => setPassword(e.target.value)}
+          value={password}
+        />
         {!loading && <input type="submit" value="Entrar" />}
-        {loading && <input type="submit" value="Aguarde..." disabled />}
+        {loading && <input type="submit" disabled value="Aguarde..." />}
         {error && <Message msg={error} type="error" />}
       </form>
-      <p>Não tem uma conta? <Link to="/register">clique aqui.</Link></p>
+      <p>
+        Não tem uma conta? <Link to="/register">Clique aqui</Link>
+      </p>
     </div>
-  )
-}
+  );
+};
 
 export default Login;
