2ecdc66c2eec905937ef149a5887f609d6bd61e4
clacerda <claudiovieira_jr@hotmail.com>
2022-12-19 17:10:15 -0300

    update profile - service

--- frontend/src/services/userService.js
+++ frontend/src/services/userService.js
@@ -13,6 +13,22 @@ const profile = async(data, token) => {
     } catch (error) {
         console.log(error)
     }
+};
+
+// Update user details
+const updateProfile = async(data, token) => {
+    const config = requestConfig("PUT", data, token, true);
+
+    try {
+        const res = await fetch(api + "/users/", config)
+                    .then((res) => res.json)
+                    .catch((err) => err);
+
+        return res;
+        
+    } catch (error) {
+        console.log(error)
+    }
 }
 
 const userService = {
