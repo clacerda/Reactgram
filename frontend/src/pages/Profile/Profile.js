3f7a3c218f93ab790d0bffe4255d6f254b4a82ea
clacerda <claudiovieira_jr@hotmail.com>
2022-12-20 18:07:16 -0300

    Photo Reducer

--- frontend/src/pages/Profile/Profile.js
+++ frontend/src/pages/Profile/Profile.js
@@ -24,6 +24,12 @@ const Profile = () => {
   const {user: userAuth} = useSelector((state) => state.auth);
 
   // Photo
+  const newPhotoForm = useRef();
+  const editPhotoForm = useRef();
+
+  const submitHandle = (e) => {
+    e.preventDefault();
+  }
 
   // Load user data
   useEffect(() => {
@@ -46,6 +52,26 @@ const Profile = () => {
             <p>{user.bio}</p>
           </div>
       </div>
+      {id === userAuth._id && (
+        <>
+        <div className="new-photo" ref={newPhotoForm}>
+          <h3>Compartilhe algum momento legal</h3>
+          <form onSubmit={submitHandle}>
+            <label>
+              <span>Título para a foto:</span> 
+              <input type="text" name="" id="" placeholder='Insira um título' />
+            </label>
+
+            <label>
+              <span>Imagem:</span> 
+              <input type="file" />
+            </label>
+
+            <input type="submit" value="Postar" />
+          </form>
+        </div>
+        </>
+      )}
     </div>
   )
 }
